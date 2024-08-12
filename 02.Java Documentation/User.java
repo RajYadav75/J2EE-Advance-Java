29aad42b42bd0f1c26c3db64c3ae89456c28410c
Raj Yadav <rajyadav751811@gmail.com>
2024-08-12 21:08:40 +0530

    Documentation of any Java Program

---
+++ 02.Java Documentation/User.java
@@ -0,0 +1,28 @@
+/**
+* This is user class
+*@author Raj
+*
+* javadoc  *.java for creation of documentation
+*/
+
+public class User
+{
+	int id;
+	String name;
+	public User(){
+	
+	}
+	
+	public User(int id, String name){
+	
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getAge(){
+		return age;
+	}
+	public String getName(){
+		return name;
+	}
+}
