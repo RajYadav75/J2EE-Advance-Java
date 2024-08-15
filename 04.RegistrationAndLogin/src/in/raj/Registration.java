359687839c72c511e76a215cd76f990c3d69a8a0
Raj Yadav <rajyadav751811@gmail.com>
2024-08-15 13:13:28 +0530

    Registration Functionallity Implemented

--- 04.RegistrationAndLogin/src/in/raj/Registration.java
+++ 04.RegistrationAndLogin/src/in/raj/Registration.java
@@ -8,28 +8,37 @@ public class Registration {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/advjdb";
     private static final String DB_UNAME = "root";
     private static final String DB_PWD = "root";
-    private static final String SQL_Query = "INSERT INTO REGISTRATION VALUES('3','raj@gmail.com','Raj','6388')";
+    private static final String SQL_Query = "INSERT INTO REGISTRATION VALUES('4','yash@gmail.com','Yash','7897')";
+    private static Connection con = null;
+    private static Statement statement = null;
 
-    public static String createTable(){
+
+    public static void connectionMaker() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(DB_URL, DB_UNAME, DB_PWD);
+        statement = con.createStatement();
+    }
+
+
+    public static String createTable() {
         final String Create_Table = "CREATE TABLE IF NOT EXISTS Registration(userID INT PRIMARY KEY AUTO_INCREMENT, Email VARCHAR(100)  ,Name VARCHAR(20),Password VARCHAR(10))";
         return Create_Table;
     }
-    public static void insertValue()throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(DB_URL, DB_UNAME, DB_PWD);
-        Statement statement = con.createStatement();
+
+
+    public static void insertValue() throws Exception {
+        connectionMaker();
         int row = statement.executeUpdate(SQL_Query);
-        System.out.println("Create Table Row Effected :: "+ row);
+        System.out.println("Create Table Row Effected :: " + row);
         con.close();
     }
-    public static void main(String[] args) throws Exception{
-//        System.out.println(createTable());
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(DB_URL, DB_UNAME, DB_PWD);
-        Statement statement = con.createStatement();
+
+    public static void main(String[] args) throws Exception {
+//        System.out.println(createTable());
+        connectionMaker();
         int creationTable = statement.executeUpdate(createTable());
-        System.out.println("Create Table Row Effected :: "+creationTable);
+        System.out.println("Create Table Row Effected :: " + creationTable);
         insertValue();
         con.close();
     }
