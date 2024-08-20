03b7122e4528cdc03d43f285e1e6a3eaca197c29
Raj Yadav <rajyadav751811@gmail.com>
2024-08-20 15:05:01 +0530

    PreparedStatement Concept

---
+++ PreparedStatement/src/in/raj/PrapareStmt.java
@@ -0,0 +1,8 @@
+package in.raj;
+
+public class PrapareStmt {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/advjdb";
+    private static final String DB_Uname = "root";
+    private static final String DB_PWD = "root";
+    private static final String SELECT_SQL = "SELECT * FROM BOOKS";
+}
